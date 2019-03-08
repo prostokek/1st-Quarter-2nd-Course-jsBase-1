ac6b00da644f89e6115147bb54792e4cddbbddda
prostokek <dimagalyanov@gmail.com>
2019-03-08 03:17:05 +0300

    Доработки

--- 6thLessonHomework.js
+++ 6thLessonHomework.js
@@ -1,11 +1,34 @@
 // ПЕРВОЕ ЗАДАНИЕ
 
-var cart = [];
+var pants = {
+    name: 'Штаны',
+    price: 40,
+    quantity: 0,
+    picture: 'https://oldskull.com.ua/image/cache/catalog/data/tur/cargo/Apache/apach-black2-800x800.jpg',
+    id: 0
+},
+    socks = {
+        name: 'Носки',
+        price: 5,
+        quantity: 0,
+        picture: 'https://mega.ru/online/upload/admitad/597/59738b00e93f9e29cbb832eb91270bb4.jpg',
+        id: 1
+    },
+    t_shirt = {
+        name: 'Футболка',
+        price: 15,
+        quantity: 0,
+        picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJTdkKjLSelgkmJJMKJ2-rB9Wfnclp-27n-7lqNlnYj94iuR9L',
+        id: 2
+    }
+
+var cart = [pants, socks, t_shirt];
 
 // ИНФОРМАЦИЯ О НАПОЛНЕННОСТИ КОРЗИНЫ
 
 function cart_printTotal(cart) { // Функция, которая выводит информацию о наполненности корзины в HTML
     document.getElementById('cart').innerHTML = '';
+
     var sum = 0; 
     var count = 0;
     for (var i = 0; i < cart.length; i++) {
@@ -13,15 +36,12 @@ function cart_printTotal(cart) { // Функция, которая выводи�
         count += cart[i].quantity;
     }
 
-    // var count = 0;
-    // for (var i = 0; i < cart.length; i++) {
-    //     count += cart[i].quantity;
-    // }
-
     var cart_total = [sum, count];
 
     var $cart_text = document.createElement('p');
     $cart_text.classList.add('cart_text'); //
+    
+    
 
     if (count == 0) {
         $cart_text.textContent = 'Корзина пуста';
@@ -45,28 +65,6 @@ console.log(cart_printTotal(cart));
 
 
 // СОЗДАНИЕ КАТАЛОГА ИЗ МАССИВА С ТОВАРАМИ
-
-var pants = {
-    name: 'Штаны',
-    price: 40,
-    quantity: 1,
-    picture: 'https://oldskull.com.ua/image/cache/catalog/data/tur/cargo/Apache/apach-black2-800x800.jpg',
-    id: 0
-},
-    socks = {
-        name: 'Носки',
-        price: 5,
-        picture: 'https://mega.ru/online/upload/admitad/597/59738b00e93f9e29cbb832eb91270bb4.jpg',
-        quantity: 1,
-        id: 1
-    },
-    t_shirt = {
-        name: 'Футболка',
-        price: 15,
-        picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJTdkKjLSelgkmJJMKJ2-rB9Wfnclp-27n-7lqNlnYj94iuR9L',
-        quantity: 1,
-        id: 2
-    }
 
 var products = [pants, socks, t_shirt];
 
@@ -108,22 +106,36 @@ buildCatalogue(products);
 
 // /СОЗДАНИЕ КАТАЛОГА ИЗ МАССИВА С ТОВАРАМИ
 
+
 var $cart = document.getElementById('cart');
 
 $catalogue.addEventListener('click', handleAddToCart_button);
 
-function handleAddToCart_button() { // Искомая функция ()
+function handleAddToCart_button() { // Искомая функция () // добавил в скобки
     if (event.target.tagName === 'BUTTON') { // если нажали на кнопку
 
         // var currentProduct_id = event.target.id; //id текущего продукта - id кнопки, на которую нажимаем
         // var currentProduct_id = event.currentTarget.id; // 
 
-        var currentProduct_id = event.target.parentNode.id; //id текущего продукта - id кнопки, на которую нажимаем
+        var currentProduct_id = +event.target.parentNode.id; //id текущего продукта - id кнопки, на которую нажимаем
 
         var currentProduct = products[currentProduct_id]; // текущий продукт - продукт с текущим id
-        cart.push(currentProduct); // добавляем этот самый продукт в корзину
+
+        // cart = [pants, socks, t_shirt];
+
+        var z = 0;
+        for (var i = 0; i < 3; i++) {
+            z++;
+            if (products[currentProduct_id].name == cart[i].name) {
+                cart[i].quantity += 1;
+            }
+        }
+        
+        // cart.push(currentProduct); // добавляем этот самый продукт в корзину
+        
 
         cart_printTotal(cart); // выводим информацию на страницу
+        console.log(cart);
     }
 }
 
